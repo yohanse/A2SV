4358aa429ab72cad0e392a0df0c84ba72be375b6
Muluken Abera <mulukenab95@gmail.com>
2022-08-28 15:13:45 +0300

    easy

--- reverse linked list.py
+++ reverse linked list.py
@@ -5,17 +5,10 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ans=[]
-        current=None
+        per=None
         while head!=None:
-            ans.insert(0,head)
-            head=head.next
-        if len(ans)==1:
-            current=ans[0]
-        else:
-            for i in range(len(ans)-1):
-                ans[i].next=ans[i+1]
-                current=ans[0]
-                ans[-1].next=None
-        return current
-            
+            temp=head.next
+            head.next=per
+            per=head
+            head=temp
+        return per
