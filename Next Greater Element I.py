14eb9dfffa78e9cd3c3480a0d1db660e54172d66
Mulle21 <mulukenab95@gmail.com>
2022-10-02 21:26:56 +0300

    easy

--- Next Greater Element I.py
+++ Next Greater Element I.py
@@ -1,23 +1,16 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        index=[]
-        length1=len(nums1)
-        length=len(nums2)
-        ans=[-1]*length
+        ans=[-1]*len(nums1)
         stack=[]
         top=-1
-        for i in range(length):
+        index={n:i for i,n in enumerate(nums1)}
+        for i in range(len(nums2)):
             while top!=-1 and stack[top]<nums2[i]:
-                ans[index.pop()]=nums2[i]
-                stack.pop()
+                val=stack.pop()
+                number=index[val]
+                ans[number]=nums2[i]
                 top=top-1
-            top=top+1
-            stack.append(nums2[i])
-            index.append(i)
-        stack=[]
-        for i in range(length1):
-            for j in range(length):
-                if nums1[i]==nums2[j]:
-                    stack.append(ans[j])
-                    break
-        return stack
+            if nums2[i] in nums1:
+                top=top+1
+                stack.append(nums2[i])
+        return ans
