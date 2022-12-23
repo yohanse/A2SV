7fc55203586864e67b4056eee1b3967b9269c6e8
Mulle21 <mulukenab95@gmail.com>
2022-12-23 17:53:40 +0300

    easy

--- swapcase.py
+++ swapcase.py
@@ -1,11 +1,7 @@
 def swap_case(s):
     res=""
     for i in s:
-        if i==i.lower():
-            res += i.upper()
-        else:
-            res += i.lower()
-            
+        res+=(i.lower() if i==i.upper() else i.upper())       
     return res
 
 if __name__ == '__main__':
