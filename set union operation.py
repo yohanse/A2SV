445ee702e5c503a5073bf275b86bbd014ae0dc28
Mulle21 <mulukenab95@gmail.com>
2022-12-25 14:39:46 +0300

    easy

---
+++ set union operation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+e = int(input())
+english = set(input().split())
+f = int(input())
+french = set(input().split())
+ans = english.union(french)
+print(len(ans))
