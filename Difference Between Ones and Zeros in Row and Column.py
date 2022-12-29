bf61c623978e8abe3b6c76a56bc7558dd2342af3
Mulle21 <mulukenab95@gmail.com>
2022-12-29 18:00:20 +0300

    medium

--- Difference Between Ones and Zeros in Row and Column.py
+++ Difference Between Ones and Zeros in Row and Column.py
@@ -1,24 +1,14 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(grid), len(grid[0])
-        res = [[-rows-cols for c in range(cols)] for r in range(rows)]
-        for c in range(1,cols):
-            grid[0][c] += grid[0][c-1]
-        for r in range(1,rows):                             # to know prefix sum
-            grid[r][0] += grid[r-1][0]
-        for r in range(1,rows):
-            for c in range(1,cols):
-                grid[r][c] += grid[r-1][c] + grid[r][c-1] - grid[r-1][c-1]
+        row, col = len(grid), len(grid[0])
 
-
-        for r in range(rows):
-            for c in range(cols):
-                if r > 0 and c > 0:
-                    res[r][c] += 2*((grid[r][-1] - grid[r-1][-1]) + (grid[-1][c] - grid[-1][c-1] ))
-                elif r > 0:
-                    res[r][c] += 2*((grid[r][-1] - grid[r-1][-1]) + grid[-1][c])   # compute result
-                elif c > 0:
-                    res[r][c] += 2*(grid[r][-1]  + (grid[-1][c] - grid[-1][c-1] ))
-                else:
-                    res[r][c] += 2*(grid[r][-1] + grid[-1][c] )
-        return res
+        res = [[0 for i in range(col+1)] for j in range(row + 1)]
+        for r in range(1,row+1):
+            for c in range(1,col+1):
+                res[r][c] = res[r-1][c] + res[r][c-1] - res[r-1][c-1] + grid[r-1][c-1]
+        
+        for r in range(1,row+1):
+            for c in range(1,col+1):
+                grid[r-1][c-1] = 2*((res[-1][c] - res[-1][c-1]) + (res[r][-1] - res[r-1][-1])) - row - col
+                
+        return grid
