8a602b1b12012441abc0b06acc80a74a560cb0df
Mulle21 <mulukenab95@gmail.com>
2023-01-26 14:07:57 +0300

    easy

--- A_Remove_Smallest.py
+++ A_Remove_Smallest.py
@@ -2,3 +2,9 @@ t = int(input())
 for _ in range(t):
     n = int(input())
     num = list(map(int, input().split()))
+    num.sort()
+    op = "YES"
+    for i in range(1,n):
+        if num[i] - num[i-1] > 1:
+            op = "NO"
+    print(op)
