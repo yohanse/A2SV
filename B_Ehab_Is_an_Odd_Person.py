8b5541787a5934ed43044d41a0ad6381511d7a3f
Mulle21 <mulukenab95@gmail.com>
2023-01-26 14:24:08 +0300

    easy

--- B_Ehab_Is_an_Odd_Person.py
+++ B_Ehab_Is_an_Odd_Person.py
@@ -1,11 +1,16 @@
 n = int(input())
 num = list(map(int, input().split()))
+check1, check2 = False, False
 for i in num:
     if i%2 == 1:
+        check1 = True
+    else:
+        check2 = True
+    if check1 and check2:
         num.sort()
         num = list(map(str,num))
         print(" ".join(num))
-        break
+        break 
 else:
     num = list(map(str,num))
     print(" ".join(num))
