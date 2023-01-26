c05da3931a6b7f936317e02b6afe39312a1c90d3
Mulle21 <mulukenab95@gmail.com>
2023-01-26 11:12:05 +0300

    easy

--- C_Less_or_Equal.py
+++ C_Less_or_Equal.py
@@ -2,10 +2,10 @@ import sys
 n, k = list(map(int, input().split()))
 num = list(map(int, input().split()))
 num.sort()
-num = [0] + num + [sys.maxsize]
+num = [1] + num + [sys.maxsize]
 
 
-if num[k + 1] - num[k] > 1 and num[k] != 10**9:
-    print(num[k] + 1)
+if num[k + 1] - num[k] > 0:
+    print(num[k])
 else:
     print(-1) 
