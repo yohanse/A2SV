cd8dfb466e4ea4c1c1d9b55fa8935daed91a8cfd
yohanse mehabaw <mehabawyohanse793@gmail.com>
2023-07-06 15:52:09 +0300

    medium

---
+++ Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        answer = 0
+        value, flag = prices[0], False
+        for price in prices[1:]:
+            if flag:
+                if price > value:
+                    answer += price - value
+                    value = price
+                else:
+                    if price + fee < value:
+                        value = price
+                        flag = False
+            else:
+                if price - fee > value:
+                    answer += price - fee - value
+                    value = price
+                    flag = True
+                else:
+                    value = min(value, price)
+        return answerclass Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        answer = 0
+        value, flag = prices[0], False
+        for price in prices[1:]:
+            if flag:
+                if price > value:
+                    answer += price - value
+                    value = price
+                else:
+                    if price + fee < value:
+                        value = price
+                        flag = False
+            else:
+                if price - fee > value:
+                    answer += price - fee - value
+                    value = price
+                    flag = True
+                else:
+                    value = min(value, price)
+        return answer
