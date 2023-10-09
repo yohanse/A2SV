6a5ba8ff50f418db6e2743ed64b4dbf8a001b7fc
yohanse mehabaw <105191845+yohanse@users.noreply.github.com>
2023-10-09 09:34:24 +0300

    added cheapest-flights-within-k-stops.python

--- cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops.py
@@ -1,15 +1,12 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        Graph={i:[] for i in range(n)}
-        for From,To,Price in flights:Graph[From].append((To,Price))
-        visite=set()
-        @cache
-        def dfs(vertex,NumberCitys):
-            if vertex==dst:return 0
-            elif NumberCitys==k+2 or vertex in visite:return sys.maxsize
-            num=sys.maxsize
-            for adjvertex,price in Graph[vertex]:
-                num=min(num,dfs(adjvertex,NumberCitys+1)+price)
-            return num
-        res=dfs(src,1)
-        return res if res!=sys.maxsize else -1
+        temp=[sys.maxsize]*n
+        temp[src]=0
+        
+        
+        for i in range(k+1):
+            Prices=temp.copy()
+            for From,To,Price in flights:
+                Prices[To]=min(Prices[To],temp[From]+Price)
+            temp=Prices
+        return temp[dst] if temp[dst]!=sys.maxsize else -1
