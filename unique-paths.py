fa68e73d454661af5460cad8d27095f421895a5f
yohanse mehabaw <105191845+yohanse@users.noreply.github.com>
2023-10-18 14:46:46 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -1,8 +1,4 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[0 for i in range(n+1)] for j in range(m+1)]
-        dp[m][n-1]=1
-        for r in range(m-1,-1,-1):
-            for c in range(n-1,-1,-1):
-                dp[r][c]=dp[r+1][c]+dp[r][c+1]
-        return dp[0][0]
+        
+        return factorial(m+n-2) // (factorial(m-1) * factorial(n-1))
